75584ff28f7872e0d5c18892da27022f3cf6e8a2
Adam Simpson <adam@heysparkbox.com>
2012-10-28 19:03:45 -0400

    Added Users/Critics labels back in

--- rt.js
+++ rt.js
@@ -8,7 +8,7 @@
     $.getJSON("http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=bfsqfcdktj8yt8bvu2d2fzq2&page_limit=5&q=" + searched + "&callback=?", function(data) {
      $.each(data.movies, function(i, movies) {
       var movie_obj = movies;
-      $('.results').append("<div class='movie'>" + "<p><a href='" + movie_obj.posters.original + "'><img src='" + movie_obj.posters.detailed + "' /></a><a class='title' target=_blank href='" + movie_obj.links.alternate + "'>" + movie_obj.title + "</a></p>" + "<div class='meta'><div class='rating'>" + movie_obj.mpaa_rating + "</div><div class='score'><p><span class='number'>" + movie_obj.ratings.critics_score + "&#37;" + "<span></p><p><span class='number'>" + movie_obj.ratings.audience_score + "&#37;" + "<span></p></div></div>");
+      $('.results').append("<div class='movie'>" + "<p><a href='" + movie_obj.posters.original + "'><img src='" + movie_obj.posters.detailed + "' /></a><a class='title' target=_blank href='" + movie_obj.links.alternate + "'>" + movie_obj.title + "</a></p>" + "<div class='meta'><div class='rating'>" + movie_obj.mpaa_rating + "</div><div class='score'><p>Critics:<span class='number'>" + movie_obj.ratings.critics_score + "&#37;" + "<span></p><p>Users:<span class='number'>" + movie_obj.ratings.audience_score + "&#37;" + "<span></p></div></div>");
     });
      cleanRT.color_code();
    });
@@ -18,7 +18,7 @@
     $.getJSON('http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=bfsqfcdktj8yt8bvu2d2fzq2&limit=5&country=us&callback=?', function(data) {
      $.each(data.movies, function(i, movies) {
       var movie_obj = movies;
-      $('.theater').append("<div class='movie'>" + "<p><a href='" + movie_obj.posters.original + "'><img src='" + movie_obj.posters.detailed + "' /></a><a class='title' target=_blank href='" + movie_obj.links.alternate + "'>" + movie_obj.title + "</a></p>" + "<div class='meta'><div class='rating'>" + movie_obj.mpaa_rating + "</div><div class='score'><p><span class='number'>" + movie_obj.ratings.critics_score + "&#37;" + "</span></p><p><span class='number'>" + movie_obj.ratings.audience_score + "&#37;" + "</span></p></div></div>");
+      $('.theater').append("<div class='movie'>" + "<p><a href='" + movie_obj.posters.original + "'><img src='" + movie_obj.posters.detailed + "' /></a><a class='title' target=_blank href='" + movie_obj.links.alternate + "'>" + movie_obj.title + "</a></p>" + "<div class='meta'><div class='rating'>" + movie_obj.mpaa_rating + "</div><div class='score'><p>Critics:<span class='number'>" + movie_obj.ratings.critics_score + "&#37;" + "</span></p><p>Users:<span class='number'>" + movie_obj.ratings.audience_score + "&#37;" + "</span></p></div></div>");
     });
      cleanRT.color_code();
    });
@@ -27,7 +27,7 @@
     $.getJSON('http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/top_rentals.json?apikey=bfsqfcdktj8yt8bvu2d2fzq2&limit=5&country=us&callback=?', function(data) {
      $.each(data.movies, function(i, movies) {
       var movie_obj = movies;
-      $('.dvd').append("<div class='movie'>" + "<p><a href='" + movie_obj.posters.original + "'><img src='" + movie_obj.posters.detailed + "' /></a><a class='title' target=_blank href='" + movie_obj.links.alternate + "'>" + movie_obj.title + "</a></p>" + "<div class='meta'><div class='rating'>" + movie_obj.mpaa_rating + "</div><div class='score'><p><span class='number'>" + movie_obj.ratings.critics_score + "&#37;" + "</span></p><p><span class='number'>" + movie_obj.ratings.audience_score + "&#37;" + "</span></p></div></div>");
+      $('.dvd').append("<div class='movie'>" + "<p><a href='" + movie_obj.posters.original + "'><img src='" + movie_obj.posters.detailed + "' /></a><a class='title' target=_blank href='" + movie_obj.links.alternate + "'>" + movie_obj.title + "</a></p>" + "<div class='meta'><div class='rating'>" + movie_obj.mpaa_rating + "</div><div class='score'><p>Critics:<span class='number'>" + movie_obj.ratings.critics_score + "&#37;" + "</span></p><p>Users:<span class='number'>" + movie_obj.ratings.audience_score + "&#37;" + "</span></p></div></div>");
     });
      cleanRT.color_code();
    });
