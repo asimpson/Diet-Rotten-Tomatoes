aae6f43f7fd3c2491cef57261c27e96abcddefb3
Adam Simpson <adam@heysparkbox.com>
2012-10-28 18:56:57 -0400

    Color coded ratings, removed backbone from head for now

--- rt.js
+++ rt.js
@@ -1,36 +1,50 @@
 (function() {
-var cleanRT = {
-	init: function(){
-		this.box_office();
-		this.top_dvd();
-	},
-	fetch: function(searched){
-		$.getJSON("http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=bfsqfcdktj8yt8bvu2d2fzq2&page_limit=5&q=" + searched + "&callback=?", function(data) {
-			$.each(data.movies, function(i, movies) {
-	        var movie_obj = movies;
-	        $('.results').append("<div class='movie'>" + "<p><a href='" + movie_obj.posters.original + "'><img src='" + movie_obj.posters.detailed + "' /></a><a class='title' target=_blank href='" + movie_obj.links.alternate + "'>" + movie_obj.title + "</a></p>" + "<div class='meta'><div class='rating'>" + movie_obj.mpaa_rating + "</div><div class='score'><p><span class='number'>" + movie_obj.ratings.critics_score + "&#37;" + "<span></p><p><span class='number'>" + movie_obj.ratings.audience_score + "&#37;" + "<span></p></div></div>");
-    		});
-		});
+  var cleanRT = {
+   init: function(){
+    this.box_office();
+    this.top_dvd();
+  },
+  fetch: function(searched){
+    $.getJSON("http://api.rottentomatoes.com/api/public/v1.0/movies.json?apikey=bfsqfcdktj8yt8bvu2d2fzq2&page_limit=5&q=" + searched + "&callback=?", function(data) {
+     $.each(data.movies, function(i, movies) {
+      var movie_obj = movies;
+      $('.results').append("<div class='movie'>" + "<p><a href='" + movie_obj.posters.original + "'><img src='" + movie_obj.posters.detailed + "' /></a><a class='title' target=_blank href='" + movie_obj.links.alternate + "'>" + movie_obj.title + "</a></p>" + "<div class='meta'><div class='rating'>" + movie_obj.mpaa_rating + "</div><div class='score'><p><span class='number'>" + movie_obj.ratings.critics_score + "&#37;" + "<span></p><p><span class='number'>" + movie_obj.ratings.audience_score + "&#37;" + "<span></p></div></div>");
+    });
+     cleanRT.color_code();
+   });
 
-	},
-	box_office: function(){
-		$.getJSON('http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=bfsqfcdktj8yt8bvu2d2fzq2&limit=5&country=us&callback=?', function(data) {
-			$.each(data.movies, function(i, movies) {
-	        var movie_obj = movies;
-	        $('.theater').append("<div class='movie'>" + "<p><a href='" + movie_obj.posters.original + "'><img src='" + movie_obj.posters.detailed + "' /></a><a class='title' target=_blank href='" + movie_obj.links.alternate + "'>" + movie_obj.title + "</a></p>" + "<div class='meta'><div class='rating'>" + movie_obj.mpaa_rating + "</div><div class='score'><p><span class='number'>" + movie_obj.ratings.critics_score + "&#37;" + "</span></p><p><span class='number'>" + movie_obj.ratings.audience_score + "&#37;" + "</span></p></div></div>");
-    		});
+  },
+  box_office: function(){
+    $.getJSON('http://api.rottentomatoes.com/api/public/v1.0/lists/movies/box_office.json?apikey=bfsqfcdktj8yt8bvu2d2fzq2&limit=5&country=us&callback=?', function(data) {
+     $.each(data.movies, function(i, movies) {
+      var movie_obj = movies;
+      $('.theater').append("<div class='movie'>" + "<p><a href='" + movie_obj.posters.original + "'><img src='" + movie_obj.posters.detailed + "' /></a><a class='title' target=_blank href='" + movie_obj.links.alternate + "'>" + movie_obj.title + "</a></p>" + "<div class='meta'><div class='rating'>" + movie_obj.mpaa_rating + "</div><div class='score'><p><span class='number'>" + movie_obj.ratings.critics_score + "&#37;" + "</span></p><p><span class='number'>" + movie_obj.ratings.audience_score + "&#37;" + "</span></p></div></div>");
+    });
+     cleanRT.color_code();
+   });
+  },
+  top_dvd: function(){
+    $.getJSON('http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/top_rentals.json?apikey=bfsqfcdktj8yt8bvu2d2fzq2&limit=5&country=us&callback=?', function(data) {
+     $.each(data.movies, function(i, movies) {
+      var movie_obj = movies;
+      $('.dvd').append("<div class='movie'>" + "<p><a href='" + movie_obj.posters.original + "'><img src='" + movie_obj.posters.detailed + "' /></a><a class='title' target=_blank href='" + movie_obj.links.alternate + "'>" + movie_obj.title + "</a></p>" + "<div class='meta'><div class='rating'>" + movie_obj.mpaa_rating + "</div><div class='score'><p><span class='number'>" + movie_obj.ratings.critics_score + "&#37;" + "</span></p><p><span class='number'>" + movie_obj.ratings.audience_score + "&#37;" + "</span></p></div></div>");
+    });
+     cleanRT.color_code();
+   });
+  },
 
-	});
-	},
-	top_dvd: function(){
-		$.getJSON('http://api.rottentomatoes.com/api/public/v1.0/lists/dvds/top_rentals.json?apikey=bfsqfcdktj8yt8bvu2d2fzq2&limit=5&country=us&callback=?', function(data) {
-			$.each(data.movies, function(i, movies) {
-		        var movie_obj = movies;
-		        $('.dvd').append("<div class='movie'>" + "<p><a href='" + movie_obj.posters.original + "'><img src='" + movie_obj.posters.detailed + "' /></a><a class='title' target=_blank href='" + movie_obj.links.alternate + "'>" + movie_obj.title + "</a></p>" + "<div class='meta'><div class='rating'>" + movie_obj.mpaa_rating + "</div><div class='score'><p><span class='number'>" + movie_obj.ratings.critics_score + "&#37;" + "</span></p><p><span class='number'>" + movie_obj.ratings.audience_score + "&#37;" + "</span></p></div></div>");
-	    	});
-	});
-	}
-}
+  color_code: function(){
+    $('.number').each(function(){
+      var theRating = $(this).html().split('%');
+      if(theRating[0] < 50){
+        $(this).addClass('rotten');
+      }
+      else{
+        $(this).addClass('fresh');
+      }
+    });
+  }
+};
 
 cleanRT.init();
 
@@ -43,7 +57,7 @@ $('#search').submit(function(e){
 });
 
 var pathArray = window.location.hash;
-if( !(pathArray == "") ){
+if( !(pathArray === "") ){
 	var escPathArray = unescape(pathArray);
 	var searchTerm = escPathArray.split('#');
 	cleanRT.fetch(searchTerm[1]);
