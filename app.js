c08145704fb5f7034e96f3396dbafd6d799507de
Adam Simpson <adam@heysparkbox.com>
2015-08-08 23:12:42 -0400

    feat: fetch movies from api and store in state

--- app.js
+++ app.js
@@ -1,9 +1,39 @@
 import React from 'react';
+import JSONP from 'browser-jsonp';
 
-class Comp extends React.Component {
+const key = 'bfsqfcdktj8yt8bvu2d2fzq2'
+const apiEndpoint = 'http://api.rottentomatoes.com/api/public/v1.0'
+const boxOfficeUrl = `${apiEndpoint}/lists/movies/box_office.json?apikey=${key}&limit=5&country=us`;
+const dvdUrl = `${apiEndpoint}/lists/dvds/top_rentals.json?apikey=${key}&limit=5&country=us`;
+//search - movies.json?apikey=bfsqfcdktj8yt8bvu2d2fzq2&page_limit=5&q=" + searched + "&callback=?"
+
+class DRT extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      'dvds': '',
+      'boxOffice': ''
+    };
+  }
+
+  componentDidMount() {
+    this.fetchMovies(boxOfficeUrl, "boxOffice")
+    this.fetchMovies(dvdUrl, "dvds")
+  }
+
+  fetchMovies (url, movieType) {
+    JSONP({
+      url: url,
+      success: (data) => { 
+        this.setState({[movieType]: data.movies});
+      }
+    });
+  }
+
   render () {
+    console.log(this.state);
     return <span>You did React!</span>
   }
 };
 
-React.render(<Comp />, document.getElementById('app'));
+React.render(<DRT />, document.getElementById('app'));
