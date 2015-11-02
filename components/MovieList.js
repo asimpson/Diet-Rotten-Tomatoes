99e4cb6175b22cbe2f5629c05679bee68770031b
Adam Simpson <adam@heysparkbox.com>
2015-11-02 18:05:57 -0500

    feat: add imdb link

--- components/MovieList.js
+++ components/MovieList.js
@@ -5,15 +5,28 @@ require('../scss/_movie-list.scss');
 const MovieList = (props) => {
 
   const movies = props.data.map((x, i) => {
+    let title;
+
     const bg = {
       backgroundImage: `url(${x.posters.detailed})`,
       padding: '1em 1em 1em 0'
     }
 
+    if (x.alternate_ids) {
+      const url = `http://m.imdb.com/title/tt${x.alternate_ids.imdb}`;
+      title = (
+        <a className="movie-link" href={url}>
+          <h1>{x.title}</h1>
+        </a>
+      );
+    } else {
+      title = <h1>{x.title}</h1>;
+    }
+    
     return (
       <div key={i} className="movie">
         <div style={bg}>
-          <h1>{x.title}</h1>
+          {title}
           <div className="movie-ratings">
             <Score name='Critics' score={x.ratings.critics_score} />
             <Score name='Audience' score={x.ratings.audience_score} />
